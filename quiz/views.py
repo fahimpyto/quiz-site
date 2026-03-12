83298ffb7172c3db68dd6df00c88fa8e2e6ff629
Fahim Ahmed <fahimahmedrudra1206@gmail.com>
2026-03-12 13:24:27 +0600

    regi veiw problem fix

--- quiz/views.py
+++ quiz/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.contrib.auth.models import User
 from django.http import JsonResponse
 import re
+
+
 def home(request):
     return render(request, 'quiz/base.html')
 
@@ -10,43 +12,31 @@ def register_view(request):
 
     if request.method == "POST":
 
-        fullname = request.POST.get("fullname")
+        fullname = request.POST.get("full_name")
         username = request.POST.get("username")
         email = request.POST.get("email")
-        password1 = request.POST.get("password1")
-        password2 = request.POST.get("password2")
+        password = request.POST.get("password")
 
-        if password1 != password2:
-            return JsonResponse({"error":"Passwords do not match"})
+        # gmail validation
+        if not re.match(r"^[a-zA-Z0-9._%+-]+@gmail\.com$", email):
+            return JsonResponse({"error": "Only Gmail allowed"})
 
+        # username check
         if User.objects.filter(username=username).exists():
-            return JsonResponse({"error":"Username already exists"})
+            return JsonResponse({"error": "Username already exists"})
 
+        # email check
+        if User.objects.filter(email=email).exists():
+            return JsonResponse({"error": "Email already exists"})
+
+        # create user
         User.objects.create_user(
             username=username,
             email=email,
-            password=password1,
+            password=password,
             first_name=fullname
         )
 
-        return JsonResponse({"success":True})
+        return JsonResponse({"success": True})
 
-    return render(request,"quiz/register.html")
-
-
-def register_view(request):
-
-    if request.method == "POST":
-
-        username = request.POST.get("username")
-        email = request.POST.get("email")
-
-        # gmail check
-        if not re.match(r"^[a-zA-Z0-9._%+-]+@gmail\.com$", email):
-            return JsonResponse({"error":"Only Gmail allowed"})
-
-        if User.objects.filter(username=username).exists():
-            return JsonResponse({"error":"Username already exists"})
-
-        if User.objects.filter(email=email).exists():
-            return JsonResponse({"error":"Email already exists"})
+    return render(request, "quiz/register.html")
