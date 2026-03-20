7e0e9d33386237239318556693a47db59aef5f50
Fahim Ahmed <fahimahmedrudra1206@gmail.com>
2026-03-20 11:46:30 +0600

    Added profile model

--- quiz/models.py
+++ quiz/models.py
@@ -60,3 +60,22 @@ class TeamMember(models.Model):
 
     def __str__(self):
         return self.name
+    
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    education = models.CharField(max_length=255, blank=True)
+
+    def __str__(self):
+        return self.user.username
+    
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+@receiver(post_save, sender=User)
+def create_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_profile(sender, instance, **kwargs):
+    instance.profile.save()
